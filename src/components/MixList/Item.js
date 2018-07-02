c7a6f0ed061119d94bfa383a1881669587c6d7c2
Marc Dingena <marc.dingena@gmail.com>
2018-07-02 21:48:31 +0200

    Display some basic mix metadata
    Is  `dangerouslySetInnerHTML` really the solution here? I feel like I'm missing something. Hope I discover a better way of doing this soon.

--- src/components/MixList/Item.js
+++ src/components/MixList/Item.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { getFeaturedImage, printTermNames } from '../../helpers/metadata';
 
 export default class extends Component {
     render() {
+        const featuredImage = getFeaturedImage( this.props._embedded['wp:featuredmedia'] );
+        const mixArtists = printTermNames( 'artist', this.props._embedded['wp:term'] );
+        const mixGenres = printTermNames( 'genre', this.props._embedded['wp:term'] );
+        const mixTags = printTermNames( 'post_tag', this.props._embedded['wp:term'] );
+        
         return(
             <div className="mix-list-item">
+                <div><img className="mix-artwork" width="54" height="54" alt="" src={ featuredImage ? featuredImage.media_details.sizes.thumbnail.source_url : 'http://placehold.it/54x54' } /></div>
                 <div><span className="mix-title" dangerouslySetInnerHTML={{ __html: this.props.title.rendered }}></span></div>
+                <div><span className="mix-artists" dangerouslySetInnerHTML={{ __html: mixArtists }}></span></div>
+                <div><span className="mix-genres" dangerouslySetInnerHTML={{ __html: mixGenres }}></span></div>
+                <div><span className="mix-tags" dangerouslySetInnerHTML={{ __html: mixTags }}></span></div>
             </div>
         );
     }
---
+++ src/helpers/metadata.js
@@ -0,0 +1,14 @@
+
+
+export const getFeaturedImage = wpFeaturedMedia => wpFeaturedMedia.length ? wpFeaturedMedia[ 0 ] : null;
+
+export const filterTerms = ( taxonomy, wpTerm ) => {
+    const matchedTermGroup = wpTerm.filter( termGroup => termGroup.length ? termGroup[0].taxonomy === taxonomy : false );
+    return matchedTermGroup.length ? matchedTermGroup[ 0 ] : [];
+};
+
+export const printTermNames = ( taxonomy, wpTerm, separator = ', ' ) => {
+    const terms = filterTerms( taxonomy, wpTerm );
+    const termNames = terms.map( term => term.name );
+    return termNames.join( separator );
+};
