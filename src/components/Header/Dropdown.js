e6a21ea4f073646e825b885dd0de18d361e0364c
Marc Dingena <marc.dingena@gmail.com>
2018-09-27 16:19:44 +0200

    Enables navigation menu toggle

--- src/components/Header/Dropdown.js
+++ src/components/Header/Dropdown.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default class extends Component {
+    constructor( props ) {
+        super( props );
+        this.state = {
+            expanded : this.props.initOpen || false
+        };
+    }
+
     render() {
         return (
-            <div className="dropdown">
-                <button><FontAwesomeIcon icon={[ 'fas', 'ellipsis-h' ]} fixedWidth /></button>
+            <div className={ `dropdown ${ this.state.expanded ? '' : 'collapsed' }` }>
+                <button onClick={ () => this.setState({ expanded : !this.state.expanded }) }><FontAwesomeIcon icon={[ 'far', 'bars' ]} fixedWidth /></button>
                 <ul>
                     {
                         this.props.children.map( ( child, index ) => <li key={ index }>{ child }</li> )
